f09ee695f45c3269ecb1794bde4fc23b769aafd2
sagar-shrestha24 <144409824+sagar-shrestha24@users.noreply.github.com>
2025-12-15 15:00:46 +0545

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -4,9 +4,9 @@ module "vpc" {
   name = "agent-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a"]
-  private_subnets = ["10.0.1.0/28"]
-  public_subnets  = ["10.0.101.0/24"]
+  azs             = ["us-east-1a","us-east-1b"]
+  private_subnets = ["10.0.1.0/28","10.0.2.0/28"]
+  public_subnets  = ["10.0.101.0/24","10.0.101.0/24"]
 
   enable_nat_gateway = false
   enable_vpn_gateway = false
