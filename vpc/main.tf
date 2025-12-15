73e0d12fde99b14c46e92a951f34199914186006
sagar-shrestha24 <144409824+sagar-shrestha24@users.noreply.github.com>
2025-12-15 14:57:52 +0545

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -5,7 +5,7 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a"]
-  private_subnets = ["10.0.1.0/24"]
+  private_subnets = ["10.0.1.0/28"]
   public_subnets  = ["10.0.101.0/24"]
 
   enable_nat_gateway = false
